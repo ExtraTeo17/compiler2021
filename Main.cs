ed8f977b81afd313e73face999021636b88a6428
Robert Pniewski <extrateo17@gmail.com>
2021-05-28 18:23:53 +0200

    Add main class file

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler2021
+{
+    class Compiler2021
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lol");
+        }
+    }
+}
