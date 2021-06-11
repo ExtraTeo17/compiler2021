0002f4fb25803640342186d79315d7232ec02d1e
Robert Pniewski <extrateo17@gmail.com>
2021-06-11 13:07:48 +0200

    Finish initial Main method

--- Main.cs
+++ Main.cs
@@ -10,9 +10,57 @@ public class Compiler
 
     public static List<string> source;
 
+    private static StreamWriter writer;
+
     public static int Main(string[] args)
     {
-        return 0;
+        string file;
+        FileStream source;
+        Console.WriteLine("\nCompiler for MINI language");
+
+        if (args.Length >= 1)
+        {
+            file = args[0];
+        }
+        else
+        {
+            Console.WriteLine("Please provide filename for compilation!");
+            return 1; // TODO: make sure it can be left like that
+        }
+
+        try
+        {
+            var reader = new StreamReader(file);
+            string fileContent = reader.ReadToEnd();
+            reader.Close();
+            Compiler.source = new List<string>(fileContent.Split(new string[] { "\r\n" }, System.StringSplitOptions.None));
+            source = new FileStream(file, FileMode.Open);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n" + e.Message);
+            return 2;
+        }
+
+        Scanner scanner = new Scanner(source);
+        Parser parser = new Parser(scanner);
+        Console.WriteLine();
+        parser.Parse();
+        source.Close();
+
+        if (errors > 0)
+        {
+            Console.WriteLine($"\n {errors} errors detected\n");
+        }
+        else
+        {
+            writer = new StreamWriter(file + ".ll");
+            GenCode();
+            writer.Close();
+            Console.WriteLine(" compilation successful\n");
+        }
+
+        return errors == 0 ? 0 : 3;
     }
 }
 
